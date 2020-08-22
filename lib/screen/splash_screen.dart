cb1d09fd750f3719bf135380979cc8dcf86249d1
GoelVaibhav009 <Creativeprogrammer623@gmail.com>
2020-08-22 22:04:03 +0530

    Change in Splash Screen

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
+import 'package:myshows/bloc/bloc.dart';
+
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
@@ -10,6 +12,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
+    contactBloc..getContacts();
     Future.delayed(Duration(seconds: 5), () {
       Navigator.pushNamed(context, '/screen0');
     });
